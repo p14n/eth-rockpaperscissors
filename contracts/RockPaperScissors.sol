7fb6294713f9e356481021063ccc46683846abc7
Dean Chapman <dean@p14n.com>
2018-06-26 16:59:29 +0100

    Tests passing

---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,152 @@
+pragma solidity ^0.4.17;
+
+contract RockPaperScissors {
+
+  function ROCK() public pure returns (uint) {
+    return 1;
+  }
+  function PAPER() public pure returns (uint) {
+    return 2;
+  }
+  function SCISSORS() public pure returns (uint) {
+    return 3;
+  }
+
+  event GameCreated(uint gameId,uint amount, address player1);
+  event GamePlayed(uint gameId,uint amount, address player1, address player2);
+  event PlayerRevealed(uint gameId,address player, uint choice);
+  event FundsWithdrawn(uint gameId,address player, uint amount);
+  event CancelRequested(uint gameId,uint fromBlock);
+
+  function firstWins(uint first,uint second) public pure returns (bool) {
+    if( first == ROCK() && second == SCISSORS()) {
+      return true;
+    } else if( first == SCISSORS() && second == PAPER()) {
+      return true;
+    } else if( first == PAPER() && second == ROCK()) {
+      return true;
+    }
+    return false;
+  }
+
+  uint gameIndex = 0;
+
+  struct Game {
+    address player1;
+    address player2;
+    uint gameId;
+    bytes32 player1Proof;
+    bytes32 player2Proof;
+    uint player1Choice;
+    uint player2Choice;
+    uint amount;
+    uint amountToPayToPlayer1;
+    uint amountToPayToPlayer2;
+    uint refundAllowedAt;
+    bool refunded;
+  }
+  mapping (uint => Game) games;
+
+  function createGame(bytes32 player1Proof) public payable returns (uint){
+
+    gameIndex = gameIndex+1;
+    uint gameId = gameIndex;
+    Game storage g = games[gameId];
+    g.gameId = gameId;
+    g.player1 = msg.sender;
+    g.player1Proof = player1Proof;
+    g.amount = msg.value;
+    emit GameCreated(gameId,g.amount,g.player1);
+    return gameId;
+
+  }
+  function playGame(uint gameId,bytes32 player2Proof) public payable {
+
+    Game storage g = games[gameId];
+    assert(g.player1 != 0);
+    assert(g.amount == msg.value);
+    assert(g.player2Proof == 0);
+    assert(player2Proof != 0);
+    assert(!g.refunded);
+    g.player2 = msg.sender;
+    g.player2Proof = player2Proof;
+    emit GamePlayed(gameId,g.amount,g.player1,g.player2);
+
+  }
+
+  function confirmChoiceOrFail(bytes32 proof,uint choice,string secret) internal pure returns (uint) {
+    if(proof != keccak256(choice,secret)){
+      revert();
+    }
+    return choice;
+  }
+
+  function reveal(uint gameId,uint choice,string playerSecret) public {
+
+    Game storage g = games[gameId];
+    assert(g.player1 != 0);
+    assert(g.player2Proof != 0);
+
+    if(g.player1 == msg.sender){
+      g.player1Choice = confirmChoiceOrFail(g.player1Proof,choice,playerSecret);
+    } else if(g.player2 == msg.sender){
+      g.player2Choice = confirmChoiceOrFail(g.player2Proof,choice,playerSecret);
+    } else {
+      revert();
+    }
+
+    if(g.player1Choice != 0 && g.player2Choice!=0){
+      if(firstWins(g.player1Choice,g.player2Choice)){
+        g.amountToPayToPlayer1 = g.amount * 2;
+      } else if(firstWins(g.player2Choice,g.player1Choice)){
+        g.amountToPayToPlayer2 = g.amount * 2;
+      } else {
+        g.amountToPayToPlayer1 = g.amount;
+        g.amountToPayToPlayer2 = g.amount;
+      }
+      g.refundAllowedAt = 0;
+    }
+
+    emit PlayerRevealed(gameId,msg.sender,choice);
+  }
+
+  function requestFunds(uint gameId) public {
+
+    Game storage g = games[gameId];
+    uint toPay;
+    if(g.player1 == msg.sender && g.amountToPayToPlayer1 > 0){
+
+      toPay = g.amountToPayToPlayer1;
+      g.amountToPayToPlayer1 = 0;
+      msg.sender.transfer(toPay);
+      emit FundsWithdrawn(gameId,msg.sender,toPay);
+
+    } else if(g.player2 == msg.sender && g.amountToPayToPlayer2 > 0){
+
+      toPay = g.amountToPayToPlayer2;
+      g.amountToPayToPlayer2 = 0;
+      msg.sender.transfer(toPay);
+      emit FundsWithdrawn(gameId,msg.sender,toPay);
+
+    } else if (g.player1 == msg.sender && g.refundAllowedAt > 0 && g.refundAllowedAt >= block.number){
+
+      g.refunded = true;
+      msg.sender.transfer(g.amount);
+      //emit FundsWithdrawn(gameId,msg.sender,g.amount);
+
+    } else {
+      revert();
+    }
+  }
+
+  function requestCancel(uint gameId) public {
+
+    Game storage g = games[gameId];
+    assert(g.player1 == msg.sender);
+    assert(g.player2Proof == 0);
+    g.refundAllowedAt = block.number + 1;
+    //emit CancelRequested(gameId,g.refundAllowedAt);
+
+  }
+
+}
---
+++ test/RockPaperScissors.js
@@ -0,0 +1,118 @@
+var RockPaperScissors =  artifacts.require("./RockPaperScissors.sol");
+var expectedExceptionPromise = require('./expected_exception_testRPC_and_geth');
+var BigNumber = require('bignumber.js');
+var utils = require('web3-utils');
+
+contract('RockPaperScissors', function ([alice,bob,carol]) {
+
+    let rps,rockVal,scissorsVal,paperVal;
+    const gameAmount = 500000000;
+    const maxGas = 500000;
+    const minWinnings = new BigNumber(gameAmount).minus(maxGas);
+
+    beforeEach('setup contract for each test', async () => {
+        rps = await RockPaperScissors.new({ from: alice })
+        rockVal = await rps.ROCK();
+        paperVal = await rps.PAPER();
+        scissorsVal = await rps.SCISSORS();
+    })
+
+    it('should assert rock beats scissors', async () => {
+        assert.isTrue(await rps.firstWins(rockVal,scissorsVal))
+    })
+    it('should assert paper beats rock', async () => {
+        assert.isTrue(await rps.firstWins(paperVal,rockVal))
+    })
+    it('should assert scissors beat paper', async () => {
+        assert.isTrue(await rps.firstWins(scissorsVal,paperVal))
+    })
+    it('should assert paper does not beat scissors', async () => {
+        assert.isFalse(await rps.firstWins(paperVal,scissorsVal))
+    })
+    it('should assert rock does not beat paper', async () => {
+        assert.isFalse(await rps.firstWins(rockVal,paperVal))
+    })
+
+    it('should allow winner to claim funds', async () => {
+
+        const balance = await web3.eth.getBalance(bob);
+        const proof1 = await utils.soliditySha3(rockVal,"pwd1");
+        const proof2 = await utils.soliditySha3(scissorsVal,"pwd2");
+
+        const tx = await rps.createGame(proof1,{ from:bob,value:gameAmount});
+        const gameId = fromLog(tx,"GameCreated","gameId").toNumber()
+        await rps.playGame(gameId,proof2,{ from:carol,value:gameAmount});
+
+        const tx2 = await rps.reveal(gameId,rockVal,"pwd1",{from:bob});
+        await rps.reveal(gameId,scissorsVal,"pwd2",{from:carol});
+        const tx3 = await rps.requestFunds(gameId,{from:bob});
+        const balance2 = await web3.eth.getBalance(bob);
+        const diff = balance2.minus(balance);
+        const gas = await gasCost(tx,tx2,tx3);
+        assert(balance.plus(gameAmount).minus(gas).eq(balance2));
+
+    })
+    it('should not allow the loser to claim funds', async () => {
+
+        const balance = await web3.eth.getBalance(carol);
+        const proof1 = await utils.soliditySha3(rockVal,"pwd1");
+        const proof2 = await utils.soliditySha3(scissorsVal,"pwd2");
+
+        const tx = await rps.createGame(proof1,{ from:bob,value:gameAmount});
+        const gameId = fromLog(tx,"GameCreated","gameId").toNumber()
+        await rps.playGame(gameId,proof2,{ from:carol,value:gameAmount});
+        await rps.reveal(gameId,rockVal,"pwd1",{from:bob});
+        await rps.reveal(gameId,scissorsVal,"pwd2",{from:carol});
+        await expectedExceptionPromise( () => rps.requestFunds(gameId,{from:carol}));
+
+    })
+    it('should prevent playing after refund', async () => {
+
+        //const balance = await web3.eth.getBalance(carol);
+        const proof1 = await utils.soliditySha3(rockVal,"pwd1");
+        const proof2 = await utils.soliditySha3(scissorsVal,"pwd2");
+
+        const tx = await rps.createGame(proof1,{ from:bob,value:gameAmount});
+        const gameId = fromLog(tx,"GameCreated","gameId").toNumber()
+        await rps.requestCancel(gameId,{ from:bob });
+        await rps.requestFunds(gameId,{from:bob});
+        await expectedExceptionPromise( () => rps.playGame(gameId,proof2,{ from:carol,value:gameAmount}));
+
+    })
+
+    it('should prevent refund after play', async () => {
+
+        const balance = await web3.eth.getBalance(carol);
+        const proof1 = await utils.soliditySha3(rockVal,"pwd1");
+        const proof2 = await utils.soliditySha3(scissorsVal,"pwd2");
+
+        const tx = await rps.createGame(proof1,{ from:bob,value:gameAmount});
+        const gameId = fromLog(tx,"GameCreated","gameId").toNumber()
+        
+        await rps.playGame(gameId,proof2,{ from:carol,value:gameAmount});
+        await expectedExceptionPromise( () => rps.requestCancel(gameId,{ from:bob }));
+
+    })
+
+
+    const gasCost = async(...txs) => {
+        let total = new BigNumber(0);
+        for (var i = 0; i < txs.length; i++) {
+           let tx = txs[i];
+           let cost = await web3.eth.getTransaction(tx.tx)
+                   .gasPrice.times(tx.receipt.gasUsed);
+            total = total.plus(cost);
+        }
+        return total;
+    }
+
+    const fromLog = (tx,event,name) => {
+        for (var i = 0; i < tx.logs.length; i++) {
+            var log = tx.logs[i];
+            if (log.event == event) {
+                return log.args[name];
+            }
+        }
+    }
+
+})
